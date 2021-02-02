cfd03f524db81f3de87fab70d6ebafedb094e46d
Henrik Karlsson <henrik10@kth.se>
2021-02-02 20:49:58 +0100

    a

--- config.def.h
+++ config.def.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -130,15 +130,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor
  */
-<<<<<<<
 unsigned int defaultfg = 7;
 unsigned int defaultbg = 258;
 static unsigned int defaultcs = 256;
-=======
-unsigned int defaultfg = 15;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 15;
->>>>>>>
 static unsigned int defaultrcs = 257;
 
 /*
